b658ab970f3aa3acf8245bc29f36b07e1e582837
SanjayMahajhan <rsmahajhan@gmail.com>
2023-11-25 06:28:45 +0000

    Reeco Solution for assignment

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+import { Provider } from "react-redux";
+import store from "./redux/Store/store";
+import { BrowserRouter as Router } from "react-router-dom";
+import ProductList from "./components/Productlist";
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <ProductList />
+        </div>
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
